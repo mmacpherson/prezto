e1a7b67f7aa9eb808f9e3bf069f076dbec6f193b
Mike Macpherson <mike.macpherson@gmail.com>
2015-09-10 11:28:51 -0700

    Add os x show/hide hidden files stuff.

--- modules/osx/init.zsh
+++ modules/osx/init.zsh
@@ -19,3 +19,9 @@ alias cdf='cd "$(pfd)"'
 
 # Pushes directory to the current Finder directory.
 alias pushdf='pushd "$(pfd)"'
+
+# Show hidden files in Finder
+alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
+
+# Hide hidden files in Finder
+alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
